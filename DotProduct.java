a212361bd18256c0362c34c436e117721abb8b13
palvarad <palvaradgh@gmail.com>
2015-11-18 11:39:22 -0500

    Added Group Information. No Other Changes

--- DotProduct.java
+++ DotProduct.java
@@ -1,3 +1,12 @@
+/**
+ * Group Member 1: Luis Velascco 
+ * Group Member 2: Larry Hong
+ * Group Member 3: Peter Alvarado Nunez
+ * Group Leader: Peter Alvarado Nunez
+ * Date: 11-18-2015
+ * Course: IT 306 - 001
+ * Class Activity 10
+ */
 package classActivity_10;
 
 import java.util.*;
